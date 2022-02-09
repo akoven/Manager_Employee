c0b26f05ce5118b0d43ecbadfeca5dff9b91f7fc
Attiya <adarensburg@live.com>
2022-02-08 18:48:09 -0600

    bonus method added to Employee class

--- employee.js
+++ employee.js
@@ -3,11 +3,30 @@ class Employee{
         this.name = name;
         this.title = title;
         this.salary = salary;
-        this.manager = boss;
+        this.boss = boss;
+
         if (boss) {
             boss.addEmployee(this);
         }
     }
+
+    bonus(mulitplier){
+        let bonusPay;
+        let sum = 0;
+        if(this.title !== 'Manager'){
+            bonusPay = this.salary*mulitplier;
+        }else{
+            let employeeArr = boss.employees;
+            for(let i = 0; i < employeeArr.length; i++){
+                sum += employeeArr[i].salary
+            }
+            bonusPay = (this.salary+sum)*mulitplier;
+        }
+        return bonusPay;
+
+    }
 }
+
+
 
 module.exports = Employee;
--- manager.js
+++ manager.js
@@ -4,17 +4,18 @@ class Manager extends Employee{
     constructor(name,title,salary){
         super(name,title,salary);
         this.employees = [];
-        
+
     }
 
     addEmployee(employee){
         this.employees.push(employee)
     }
 
+
 }
 
-let annie = new Manager('Annie', 100000, 'Director')
-let alvy = new Employee('Alvy', 75000, 'Analyst', annie)
+let annie = new Manager('Annie', 'Director', 100000)
+let alvy = new Employee('Alvy', 'Analyst', 75000, annie)
 
 
 console.log(annie)
